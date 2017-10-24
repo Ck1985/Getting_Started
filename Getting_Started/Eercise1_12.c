a31ec5afb5c2b6787f64172fbf2cacbb5a4b47ec
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-10-24 23:07:14 +0700

    New Commit

---
+++ Getting_Started/Eercise1_12.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+main() {
+	int c, nc, nl, nw, nt, ns, state;
+
+	nc = nl = nw = nt = ns = 0;
+	state = OUT;
+
+	while ((c = getchar()) != EOF) {
+		++nc;
+		if (c == '\n') {
+			++nl;
+		}
+		if (c == ' ') {
+			++ns;
+		}
+		if (c == '\t') {
+			++nt;
+		}
+		if ((c == ' ') || (c == '\t') || (c == '\n')) {
+			state = OUT;
+		} 
+		else if (state == OUT) {
+			state = IN;
+			ns = nt = 0;
+			++nw;
+		}
+		if (state == IN) {
+			putchar(c);
+		}
+		else if ((ns == 1) || (nt == 1)) {
+			putchar('\n');
+		}
+	}
+}
--- Getting_Started/Word_Counting.c
+++ Getting_Started/Word_Counting.c
@@ -3,7 +3,7 @@
 #define IN 1    /*Inside a word*/
 #define OUT 0   /*Outside a word*/
 
-main() {
+mainCount() {
 	int c, nl, nw, nc, state;
 
 	c = getchar();
