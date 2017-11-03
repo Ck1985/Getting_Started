2ea5dfb8ba9f936ce56c43d16f3431a930dd9bc6
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-11-04 00:22:54 +0700

    New Commit

--- Getting_Started/CharacterArrays.c
+++ Getting_Started/CharacterArrays.c
@@ -2,9 +2,38 @@
 
 #define MAXLINE 1000  /* Max input line size*/
 
+int getLine(char s[], int maxline);
+void copy(char to[], char from[]);
+
 /* Print maximum input longest */
 int main() {
+	int len;                  // current line length
+	int max;                  // maximum length seen so far
+	char line[MAXLINE];       // current input line
+	char longest[MAXLINE];    // longest line saved here
+
 
 }
 
 /* getLine: Read a line into s, return length */
+int getLine(char s[], int lim) {
+	int c, i;
+	for (i = 0; (i < lim - 1) && (c = getchar() != EOF) && (c != '\n'); ++i) {
+		s[i] = c;
+	}
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* copy: copy "from" into "to": assume to is big enough */
+void copy(char to[], char from[]) {
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0') {
+		++i;
+	}
+}
