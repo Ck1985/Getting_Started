ea0b93db3a977bef5fd09f3cbfa186c6b459c1cf
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-11-09 12:30:13 +0700

    New Commit

--- Getting_Started/Exercise1_20.c
+++ Getting_Started/Exercise1_20.c
@@ -4,10 +4,11 @@
 #include <stdio.h>
 
 #define MAXLINE 1000
-#define TABINC 8
+#define TABIN	C 8
 
 int index = 0;
 char inputLine[MAXLINE];
+char locationTab[MAXLINE];
 char editedLine[MAXLINE];
 
 int getLine1_20(void);
@@ -16,7 +17,7 @@ void processTabs1_20(void);
 int main() {
 	getLine1_20();
 	processTabs1_20();
-	printf("%s \n", editedLine);
+	printf("%s\n", locationTab);
 	return 0;
 }
 
@@ -36,31 +37,67 @@ int getLine1_20(void) {
 }
 
 void processTabs1_20(void) {
-	int i, j = 0, countTabs;
+	int i, j = 0, length, startTab, stopTab;
 	extern int index;
-	extern char inputLine[], editedLine[];
+	extern char inputLine[], editedLine[], locationTab[];
 
 	while (inputLine[index] != '\n') {
 		index++;
 	}
+	length = index;
 
-	for (i = 0; i < index; ++i) {
-		if (inputLine[i] == '\t') {
-			countTabs = 1;
-			while (inputLine[++i] == '\t') {
-				++countTabs;
-			}
-			--i;
-			for (int k = 1; k <= countTabs; k++) {
-				for (int l = 0; l < TABINC; l++) {
-					editedLine[j] = '#';
-					++j;
-				}
-			}
-		}
-		else {
-			editedLine[j] = inputLine[i];
-			++j;
+	for (i = 0; i < length; ++i) {
+		locationTab[i] = '0';
+	}
+	locationTab[++i] = '\n';
+	locationTab[++i] = '\0';
+	i = 0;
+
+	for (int k = 0; k < length; ++k) {
+		if (inputLine[k] == '\t') {
+			locationTab[k] = '1';
 		}
 	}
+
+	for (i = 0; i < length; ++i) {
+
+	}
 }
+
+/*#include<stdio.h>
+#define TABINC 8
+
+int main(void)
+{
+	int nb, pos, c;
+
+	nb = 0;
+	pos = 1;
+
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\t')
+		{
+			nb = TABINC - ((pos - 1) % TABINC);
+
+			while (nb > 0)
+			{
+				putchar('#');
+				++pos;
+				--nb;
+			}
+		}
+		else if (c == '\n')
+		{
+			putchar(c);
+			pos = 1;
+		}
+		else
+		{
+			putchar(c);
+			++pos;
+		}
+	}
+
+	return 0;
+}*/
