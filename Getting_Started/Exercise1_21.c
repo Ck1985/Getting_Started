db91cf2ef2e317b31bc528db1e1d5bf4dc0fe397
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-11-11 22:06:01 +0700

    New Commit

--- Getting_Started/Exercise1_21.c
+++ Getting_Started/Exercise1_21.c
@@ -33,43 +33,39 @@ int getLine1_21(void) {
 }
 
 void processLine1_21(void) {
-	int i, j = 0, countBlanks, genBlanks, even, odd, index = 0;
-	int startTab, stopTab, flagTab = 1, phan_bu = 0;
-	extern char processedLine[];
+	int i, j = 0, headTab = 0, tailTab, countBlanks;
+	int index = 0, firstFlag = 1, fraction = 0, countCharacter = 0, phan_bu = 0;
+	int oldValue = 0;
+	extern char inputLine[], processedLine[];
 
 	while (inputLine[index] != '\n') {
 		index++;
 	}
 	length = index;
+
 	for (i = 0; i < length; ++i) {
 		if ((inputLine[i] == ' ') || (inputLine[i] == '\b')) {
 			countBlanks = 1;
 			while ((inputLine[++i] == ' ') || (inputLine[++i] == '\b')) {
 				++countBlanks;
 			}
-			if (countBlanks >= TABINC) {
-					if ((countBlanks % TABINC) == 0) {
-						even = countBlanks / TABINC;
-							for (int k = 1; k <= even + phan_bu; ++k) {
-								processedLine[j++] = '\t';
-							}											
-					}
-					else {							
-							even = countBlanks / TABINC;
-							odd = countBlanks % TABINC;
-							for (int m = 1; m <= even; ++m) {
-								processedLine[j++] = '\t';
-							}
-							for (int n = 1; n <= odd + phan_bu; ++n) {
-								processedLine[j++] = ' ';
-							}
-					}
-			}
 			i = i - 2;
+			if (countBlanks >= 8) {
+				/* Most difficult part of program */
+				
+				/*--------------------------------*/
+			}
+			else {
+				processedLine[j] = inputLine[i];
+				++j;
+				++countCharacter;
+			}
 		}
 		else {
 			processedLine[j] = inputLine[i];
 			++j;
+			++countCharacter;
 		}
 	}
 }
+
